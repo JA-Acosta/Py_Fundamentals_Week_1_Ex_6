846472eccd0b891b43ea23a20c754619bf835e55
JAAR <joe.aaron.acosta@outlook.com>
2023-07-14 21:23:54 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+>>> JAAR
+>>> 07/14/2023
+>>> Practicing Fundamentals Program 6
+>>> Version 0
+'''
+'''
+>>> Generates a program that will take a phrase and count the total words in the phrase, total consonants (always including y), and the vowels (always excluding y). The program will then use the word count total as a size for the base of a pyramid that the program will build. Afterwards, the program will ask for a second sentence to determine if both sentences are anagrams.
+'''
+
+def main() :
+    print('Hello World!')
+
+if __name__ == '__main__' :
+    main()
